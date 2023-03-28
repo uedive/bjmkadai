9559b52157bd57a78247b0115aa71d8e91e5e8e0
uedive <uedive@gmai.com>
2023-03-28 18:49:35 +0900

    Init

---
+++ src/lib/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = 'https://opendata.resas-portal.go.jp';
+
+const PREFECTURES_ENDPOINT = '/api/v1/prefectures';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'X-API-KEY': process.env.REACT_APP_API_KEY,
+    'Content-Type': 'application/json',
+  },
+});
+
+export async function fetchPrefectures() {
+  const response = await api.get(PREFECTURES_ENDPOINT);
+  const prefectures = response.data.result;
+  return prefectures;
+}
